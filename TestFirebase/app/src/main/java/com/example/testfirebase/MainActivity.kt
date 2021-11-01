d45fa4a3e43663c93c351cb983b93466ee361bf3
Vorfall <alekseevegorc@gmail.com>
2021-11-01 21:25:45 +0300

    test, you better not look

--- TestFirebase/app/src/main/java/com/example/testfirebase/MainActivity.kt
+++ TestFirebase/app/src/main/java/com/example/testfirebase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.testfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dbRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("TestGroup")
+        var but : Button = findViewById(R.id.button)
+        but.setOnClickListener{
+            var id = dbRef.getKey()
+            var text = "Test TEXT".toString()
+            var id1 = id ?: "0"
+            var test1: TestData = TestData(id1,text)
+            dbRef.push().setValue(test1)
+        }
+
+
     }
 }
---
+++ TestFirebase/app/src/main/java/com/example/testfirebase/TestData.kt
@@ -0,0 +1,6 @@
+package com.example.testfirebase
+
+class TestData(_key:String , _text: String){
+    var key = _key
+    var text = _text
+}
